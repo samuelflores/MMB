b1f1fb81694629e532e46f644371bde061e04099
samuelflores <samuelfloresc@gmail.com>
2021-05-11 13:44:27 +0200

    made some changes to concatenate-DNA-base-pairs.sh, so now it takes care of adding a phosphorus, moving it to the beginning of the residue, etc.

--- scripts/concatenate-DNA-base-pairs.sh
+++ scripts/concatenate-DNA-base-pairs.sh
@@ -15,3 +15,9 @@ grep -h DT last.????.pdb >> DT.pdb
 
 cat DA.pdb > DNA-spiral.pdb
 tac DT.pdb >> DNA-spiral.pdb
+
+sed -i 's/H5T/P  /g' DNA-spiral.pdb 
+awk '{if ($3 == "P"){print $5"P"$2,$0} else {print $5"Z"$2,$0}}' DNA-spiral.pdb > temp.2.pdb
+sort -k1 temp.2.pdb > temp.3.pdb
+cut -b 13-90 temp.3.pdb > DNA-spiral.pdb
+rm temp.2.pdb temp.3.pdb DA.pdb DT.pdb
